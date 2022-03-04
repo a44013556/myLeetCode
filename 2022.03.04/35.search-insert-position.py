227498d8cf1d0cbff49962125823e5118b500493
a44013556 <a44013556@gmail.com>
2022-03-04 20:11:09 +0900

    Delete the code that do not need

--- 2022.03.04/35.search-insert-position.py
+++ 2022.03.04/35.search-insert-position.py
@@ -12,8 +12,6 @@ class Solution:
         return nums.index(target)
 
     def searchInsert2(self, nums: List[int], target: int) -> int:
-        if target < nums[0]: return 0
-
         for i ,num in enumerate(nums):
             if num >= target:
                 return i
